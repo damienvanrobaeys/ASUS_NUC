7008d4b0c7be8fcd86e3a70647912891e0856d61
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2025-03-10 10:59:13 +0100

    Add files via upload

--- ASUS_NUC_BIOSSettings_Parser.ps1
+++ ASUS_NUC_BIOSSettings_Parser.ps1
@@ -1,41 +1,67 @@
+param(
+[switch]$Grid,
+[string]$CSV,
+[string]$TXT
+)
+
+If($TXT -ne $null)
+	{
+
+	}
+	
+If($CSV -ne $null)
+	{
+
+	}	
+
+$iSetupCfg_URL_FromGitHub = "https://github.com/damienvanrobaeys/Intel_NUC/blob/main/iSetupCfgWin64.exe"
+$amigendrv64_URL_FromGitHub = "https://github.com/damienvanrobaeys/Intel_NUC/blob/main/amigendrv64.sys"
+
 <#
 If you want to use iSetupCfgWin64.exe and amigendrv64.sys from a blob storage:
-
 1. Upload both files iSetupCfgWin64.exe and amigendrv64.sys on a blob storage or somewhere
 2. Set path of iSetupCfgWin64.exe in variable iSetupCfg_URL
 3. Set path of amigendrv64.sys in variable amigendrv64_URL
 #>
 
-<#
-If you want to use iSetupCfgWin64.exe and amigendrv64.sys locally:
-1. Create a folder C:\Windows\temp\Config_BIOS
-2. Copy iSetupCfgWin64.exe in C:\Windows\temp\Config_BIOS
-3. Copy amigendrv64.sys in C:\Windows\temp\Config_BIOS
-#>
+$iSetupCfg_URL = ""
+$amigendrv64_URL = ""
 
-$iSetupCfg_URL = "https://github.com/damienvanrobaeys/Intel_NUC/blob/main/iSetupCfgWin64.exe"
-$amigendrv64_URL = "https://github.com/damienvanrobaeys/Intel_NUC/blob/main/amigendrv64.sys"
-
-$Config_BIOS_folder = "C:\Windows\temp\Config_BIOS"
-If(!(test-path $Config_BIOS_folder)){new-item $Config_BIOS_folder -Type Directory -Force}		
-$iSetupCfg_OutFile = "$Config_BIOS_folder\iSetupCfgWin64.exe"
-If(!(test-path $iSetupCfg_OutFile))
+If($iSetupCfg_URL -eq "")
 	{
-		Invoke-WebRequest -Uri $iSetupCfg_URL -OutFile $iSetupCfg_OutFile -UseBasicParsing
+		$iSetupCfg_URL = $iSetupCfg_URL_FromGitHub
 	}
-		
-$amigendrv64_OutFile = "$Config_BIOS_folder\amigendrv64.sys"
-If(!(test-path $amigendrv64_OutFile))
+	
+If($amigendrv64_URL -eq "")
 	{
-		Invoke-WebRequest -Uri $amigendrv64_URL -OutFile $amigendrv64_OutFile -UseBasicParsing
-	}			
-		
-Get-ChildItem -Recurse $Config_BIOS_folder | Unblock-File
-		
-$ComputerName = $env:computername
-$Exported_Config_BIOS = "C:\Windows\Temp\Config_BIOS\Config_BIOS_$ComputerName.txt"
+		$amigendrv64_URL = $amigendrv64_URL_FromGitHub
+	}	
 
-& "C:\Windows\Temp\Config_BIOS\iSetupCfgWin64.exe" /o /s $Exported_Config_BIOS /b /q			
+$ComputerName = $env:computername			
+If($TXT -ne $null)
+	{
+		$Exported_Config_BIOS = $TXT
+	}	
+Else
+	{
+		$Config_BIOS_folder = "C:\Windows\temp\Config_BIOS"
+		If(!(test-path $Config_BIOS_folder)){new-item $Config_BIOS_folder -Type Directory -Force}		
+		$iSetupCfg_OutFile = "$Config_BIOS_folder\iSetupCfgWin64.exe"
+		If(!(test-path $iSetupCfg_OutFile))
+			{
+				Invoke-WebRequest -Uri $iSetupCfg_URL -OutFile $iSetupCfg_OutFile -UseBasicParsing
+			}
+				
+		$amigendrv64_OutFile = "$Config_BIOS_folder\amigendrv64.sys"
+		If(!(test-path $amigendrv64_OutFile))
+			{
+				Invoke-WebRequest -Uri $amigendrv64_URL -OutFile $amigendrv64_OutFile -UseBasicParsing
+			}			
+				
+		Get-ChildItem -Recurse $Config_BIOS_folder | Unblock-File		
+		$Exported_Config_BIOS = "C:\Windows\Temp\Config_BIOS\Config_BIOS_$ComputerName.txt"		
+		& "C:\Windows\Temp\Config_BIOS\iSetupCfgWin64.exe" /o /s $Exported_Config_BIOS /b /q				
+	}
 
 $BIOS_Content = gc $Exported_Config_BIOS| Where {(($_ -like "*=*") -and ($_ -notlike "*Help String*")`
 -and ($_ -notlike "*Token*") -and ($_ -notlike "*Offset*=*")`
@@ -54,7 +80,6 @@ For($i = 0; $i -lt $BIOS_Content.Count; $i += 3){
 	$value = ($value.split("="))[1]
 
     $item = [PSCustomObject]@{
-        DeviceName = $ComputerName
         Setting = $question
         Map = $map
         Value = $value
@@ -62,4 +87,13 @@ For($i = 0; $i -lt $BIOS_Content.Count; $i += 3){
     $BIOS_Settings += $item
 }
 
-$BIOS_Settings | out-gridview
+If(($Grid) -or (($CSV -eq "") -and (!($Grid))))
+	{
+		$BIOS_Settings | out-gridview		
+	}
+	
+If($CSV -ne "")
+	{
+		$CSV_FileName = "Config_BIOS_$ComputerName.csv"
+		$BIOS_Settings | export-csv "$CSV\$CSV_FileName" -NoTypeInformation
+	}	
